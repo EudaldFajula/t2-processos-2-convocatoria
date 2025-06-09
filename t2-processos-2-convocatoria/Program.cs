c1cb38ec8317c1750b6c857a743e3b44588f3586
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-06-09 18:48:39 +0200

    Add project files.

---
+++ t2-processos-2-convocatoria/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace t2_processos_2_convocatoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
